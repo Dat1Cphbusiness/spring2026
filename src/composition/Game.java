394e2ad9a6db932157c75ef4fa3b07a42d8f998c
KongCPH <kong@ek.dk>
2026-03-02 10:23:38 +0100

    Pseudokode og IO klasse

--- src/composition/Game.java
+++ src/composition/Game.java
@@ -1,6 +1,39 @@
 package composition;
 
 public class Game {
+    private int answer;
+    private boolean quit = false;
 
+    public void startGame(){
+        // Velkommen til bruger (regler: skriv q for at stoppe)
+        // Spørg bruger om navn
+        // Vi skal bruge IO til dette
+
+        // Vil du spille spil y/n
+
+        // så længe brugerinput er y så kør nyt spil
+
+        // Opret et answer
+
+        // start runde
+
+    }
+
+
+    private void playRound(){
+        // Bed spiller om gæt
+        // Brug IO om dette
+        // Tre muligheder: korrekt, for højt, for lavt
+        // Hold styr på antal gæt
+
+        // Hvis korrekt: Fortæl bruger det er korrekt
+        // Tildel score til Player
+        // Returner til startGame
+
+        // Hvis det er for lavt eller for højt
+        // Fortæl bruger om det er for højt eller for lavt
+        // Giv bruger mulighed for at gætte igen eller afbryde spil
+
+    }
 
 }
---
+++ src/composition/IO.java
@@ -0,0 +1,27 @@
+package composition;
+
+import java.util.Scanner;
+
+public class IO {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void sendMessage(String msg){
+        printHashTags();
+        System.out.println(msg);
+        printHashTags();
+    }
+
+    public String promptString(String msg){
+        printHashTags();
+        System.out.println(msg);
+        String input = scanner.nextLine();
+        printHashTags();
+        return input;
+    }
+
+    private void printHashTags(){
+        System.out.println("###############");
+    }
+
+}

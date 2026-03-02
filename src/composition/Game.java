628badf5399668d7a39ab537f2d1f3ee427498f7
KongCPH <kong@ek.dk>
2026-03-02 11:12:58 +0100

    startGame

--- src/composition/Game.java
+++ src/composition/Game.java
@@ -1,5 +1,7 @@
 package composition;
 
+import java.util.Random;
+
 public class Game {
     private int answer;
     private boolean quit = false;
@@ -10,20 +12,23 @@ public class Game {
         // Velkommen til bruger (regler: skriv q for at stoppe)
         io.sendMessage("Velkommen til \n \"Gæt et tal\" ");
         // Spørg bruger om navn
+        String playerName = io.promptString("Hvad er dit navn");
+        player = new Player(playerName);
         // Vi skal bruge IO til dette
 
         // Vil du spille spil y/n
-
-        // så længe brugerinput er y så kør nyt spil
-
-        // Opret et answer
-
-        // start runde
-
+        String input = io.promptString("Vil du spille et nyt spil?");
+        // hvis brugerinput er y så kør nyt spil
+        if(input.equals("y")){
+            io.sendMessage("Gæt et tal mellem 1 og 100");
+            // Opret et answer
+            Random random = new Random();
+            answer = random.nextInt(100) + 1;
+            playGame();
+        }
     }
 
-
-    private void playRound(){
+    private void playGame(){
         // Bed spiller om gæt
         // Brug IO om dette
         // Tre muligheder: korrekt, for højt, for lavt
